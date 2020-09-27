0ae64a9037807d313ed55dac9c485b58c3b14308
Pam Forrest <69604137+pmf1020@users.noreply.github.com>
2020-09-28 09:00:54 +1300

    Delete MonthConverter.java

--- chapter6/MonthConverter.java
+++
@@ -1,9 +0,0 @@
-package chapter6;
-
-public class MonthConverter {
-
-    public static void main(String args[]){
-        System.out.println(Month.getMonth(2));
-        System.out.println(Month.getMonth("January"));
-    }
-}
